339a9fe566b51af584e1475554e2c1eeaf4f11a5
mirodil1 <ortikov.m97@gmail.com>
2022-10-18 17:19:31 +0500

    moved to other components

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
   <div class="wrapper">
     <Header/>
-    <!-- <About
-    /> -->
     <router-view/>
   </div>
 </template>
 
 <script>
 import Header from './components/WordleHeader.vue';
-// import About from './components/WordleAbout.vue'
-// import { toast } from 'bulma-toast'
 
 export default {
   name: 'App',
   components: {
     Header,
-    // About,
   },
 }
 </script>
@@ -35,19 +30,12 @@ export default {
   @media only screen and (max-width: 500px) {
     .info {
         width: 370px;
-        /* line-height: 3rem;
-        text-align: center;
-        font-size: 25px; */
     }
   }
   @media only screen and (min-width: 320px) 
                        and (max-width: 360px){
     .info {
         width: 340px;
-        /* box-sizing: border-box;
-        text-align: center;
-        line-height: 20px;
-        font-size: 20px; */
     }
   }
 
